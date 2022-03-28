ad6912202d26a9433890e69fa6e5262035ea99de
dinah <dinahnovema18@gmail.com>
2022-03-28 16:35:20 +0200

    Added loader

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 const adviceId = document.getElementById("adviceId");
 const advice = document.getElementById("advice");
 const newAdvice = document.getElementById("circle");
+const loading = document.getElementById("loading");
+loading.classList.remove("loading");
 
 const showAdvices = async () => {
+  loading.classList.add("loading");
   try {
     const data = await fetch("https://api.adviceslip.com/advice");
     const response = await data.json();
 
     adviceId.innerHTML = `ADVICE #${response.slip.id}`;
     advice.innerHTML = `"${response.slip.advice}"`;
+    loading.style.display = "none";
   } catch (err) {
     adviceId.innerHTML = "Error";
     advice.innerHTML = "Failed to load advice. Please try again";
